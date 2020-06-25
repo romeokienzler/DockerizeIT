69f702b132e1cff8d2f10828cbac2d6d72424648
Romeo Kienzler <romeokienzler@oc3886434834.ibm.com>
2020-06-25 17:14:32 +0200

    added ibmcloud and audacity

---
+++ audacity/run.sh
@@ -0,0 +1,3 @@
+sudo xhost +
+docker run -it --net=host --ipc=host -e DISPLAY=$DISPLAY -v /tmp/shared:/shared --workdir /shared -v /tmp/.X11-unix:/tmp/.X11-unix --env="QT_X11_NO_MITSHM=1" --device /dev/snd  audacity
+
