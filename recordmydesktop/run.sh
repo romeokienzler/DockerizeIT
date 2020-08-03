f70a9336506234fca2ca8b7024363b9df7bcf55a
Romeo Kienzler <romeokienzler@oc3886434834.ibm.com>
2020-08-03 19:54:02 +0200

    .

--- recordmydesktop/run.sh
+++ recordmydesktop/run.sh
@@ -1,3 +1,3 @@
 sudo xhost +
-docker run -it --net=host --ipc=host -e DISPLAY=$DISPLAY -v /tmp/shared:/shared --workdir /shared -v /tmp/.X11-unix:/tmp/.X11-unix --env="QT_X11_NO_MITSHM=1" recordmydesktop
+docker run -it --net=host --device /dev/snd --ipc=host -e DISPLAY=$DISPLAY -v /tmp/shared:/shared --workdir /shared -v /tmp/.X11-unix:/tmp/.X11-unix --env="QT_X11_NO_MITSHM=1" recordmydesktop
 
