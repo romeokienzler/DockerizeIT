7ca4e8cb02c33e5356db32a81966d4888678a8ed
Romeo Kienzler <romeokienzler@oc3886434834.ibm.com>
2020-08-14 10:23:35 +0200

    ffmpeg

---
+++ ffmpeg/run.sh
@@ -0,0 +1,3 @@
+#sudo chcon -Rt svirt_sandbox_file_t /tmp/shared
+#docker run -it -v /tmp/shared:/shared --workdir /shared --user $(id -u):$(id -g) youtube-dl
+docker run -it -v /tmp/shared:/shared:Z --workdir /shared ffmpeg
