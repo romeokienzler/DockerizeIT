5747b1279bc61c8253442275d0fe78c42b1f63d8
Romeo Kienzler <romeokienzler@oc3886434834.ibm.com>
2020-08-19 11:55:09 +0200

    progress on elyra and obs

--- elyra-ai/run.sh
+++ elyra-ai/run.sh
@@ -1,1 +1,3 @@
-docker run -it -p 8888:8888/tcp elyra-ai
+id=`docker run -d -p 8888:8888 -v elyra:/home/jovyan/work elyra-ai jupyter-lab`
+sleep 3
+docker exec -it $id jupyter notebook list |sed -s 's/0.0.0.0/127.0.0.1/'
