505f5a92e6962664d6ab24f8db3aa6113d6c947b
Romeo Kienzler <romeo.kienzler@gmail.com>
2020-08-23 22:26:48 +0200

    Update run.sh

--- elyra-qiskit/run.sh
+++ elyra-qiskit/run.sh
@@ -1,3 +1,1 @@
-id=`docker run -d -p 8888:8888 -v elyra:/home/jovyan/work elyra-ai jupyter-lab`
-sleep 3
-docker exec -it $id jupyter notebook list |sed -s 's/0.0.0.0/127.0.0.1/'
+docker run -it -p 8888:8888 -v elyra-qiskit jupyter-lab
