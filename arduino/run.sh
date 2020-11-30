bba3409a20405a9a610702b8feb10997c40845a3
Romeo Kienzler <romeokienzler@oc3886434834.ibm.com>
2020-11-30 06:29:32 +0100

    arduino, ffmpeg yubico

---
+++ arduino/run.sh
@@ -0,0 +1,4 @@
+sudo xhost +
+#docker run -it --net=host --ipc=host -e DISPLAY=$DISPLAY -v /tmp/shared:/shared --workdir /shared -v /tmp/.X11-unix:/tmp/.X11-unix --env="QT_X11_NO_MITSHM=1" --device /dev/snd  audacity
+docker run -it --net=host --ipc=host -e DISPLAY=$DISPLAY -v /home/romeokienzler:/shared --workdir /shared -v /tmp/.X11-unix:/tmp/.X11-unix --env="QT_X11_NO_MITSHM=1" arduino
+
--- ffmpeg/run.sh
+++ ffmpeg/run.sh
@@ -1,4 +1,8 @@
 #sudo chcon -Rt svirt_sandbox_file_t /tmp/shared
 #docker run -it -v /tmp/shared:/shared --workdir /shared --user $(id -u):$(id -g) youtube-dl
 # 
+#normalize
+#ffmpeg -i 1.ogv -af "volumedetect" -vn -sn -dn -f null /dev/null
+## ffmpeg -i 2.mp4 -af "volume=6.1dB" -c:v copy -c:a aac -b:a 192k 2norm.mp4
+
 docker run -it -v /home/romeokienzler/proj:/shared:Z --workdir /shared ffmpeg
