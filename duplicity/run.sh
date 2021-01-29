fcd7630e5e7a5da07a13f6c1933933729f1f183d
Romeo Kienzler <romeokienzler@oc3886434834.ibm.com>
2021-01-29 10:13:29 +0100

    some changes

--- duplicity/run.sh
+++ duplicity/run.sh
@@ -2,6 +2,6 @@
 #AWSSecretKey=<your secret key>
 #duplicity s3://s3.eu-de.cloud-object-storage.appdomain.cloud/backup-test-duplicity-deleteme .
 #duplicity proj s3://s3.eu-de.cloud-object-storage.appdomain.cloud/rkie-backup/
-#duplicity --allow-source-mismatch proj/1codait s3://s3.eu-de.cloud-object-storage.appdomain.cloud/rkie-backup/1codait/
+#duplicity --allow-source-mismatch proj/ s3://s3.eu-de.cloud-object-storage.appdomain.cloud/rkie-backup/1codait/
 docker run -it -v /home/romeokienzler:/shared:Z --workdir /shared -v /home/romeokienzler/cred:/cred:Z --workdir /shared duplicity
 
--- ibmonenlp/run.sh
+++ ibmonenlp/run.sh
@@ -1,4 +1,3 @@
-
-
-docker run -it ibmonenlp /bin/bash
+source creds
+docker run -it -e ARTIFACTORY_USERNAME -e ARTIFACTORY_API_KEY -v ibmonenlp_work:/home/jovyan/work -p 8888:8888 ibmonenlp jupyter lab
 
