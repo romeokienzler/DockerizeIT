5b794f08690a80508e9f11733847a360589a138f
Romeo Kienzler <romeokienzler@oc3886434834.ibm.com>
2021-02-16 15:21:21 +0100

    latest changes

--- ffmpeg/run.sh
+++ ffmpeg/run.sh
@@ -4,5 +4,6 @@
 #normalize
 #ffmpeg -i 1.ogv -af "volumedetect" -vn -sn -dn -f null /dev/null
 ## ffmpeg -i 2.mp4 -af "volume=6.1dB" -c:v copy -c:a aac -b:a 192k 2norm.mp4
+#    ffmpeg -i myvideo.mp4 myaudio.mp3 
 
 docker run -it -v /home/romeokienzler/proj:/shared:Z --workdir /shared ffmpeg
--- ibmonenlp/run.sh
+++ ibmonenlp/run.sh
@@ -1,3 +1,4 @@
 source creds
-docker run -it -e ARTIFACTORY_USERNAME -e ARTIFACTORY_API_KEY -v ibmonenlp_work:/home/jovyan/work -p 8888:8888 ibmonenlp jupyter lab
+docker run -it -e ARTIFACTORY_USERNAME -e ARTIFACTORY_API_KEY -v ibmonenlp_work:/home/jovyan/work -p 8889:8889 ibmonenlp jupyter lab --port 8889
+
 
