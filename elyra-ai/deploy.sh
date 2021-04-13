c199904a0767fc2db212f28c41e339d8f663741d
Romeo Kienzler <romeo.kienzler@ch.ibm.com>
2021-04-13 12:10:44 +0200

    elyra on kube and kind

---
+++ elyra-ai/deploy.sh
@@ -0,0 +1,11 @@
+kind create cluster --config=kind.yaml
+kubectl apply -f pv.yaml
+kubectl apply -f pvc.yaml
+kubectl apply -f pod_pvc.yaml
+kubectl apply -f service.yaml
+kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/master/deploy/static/provider/kind/deploy.yaml
+kubectl wait --namespace ingress-nginx \
+  --for=condition=ready pod \
+  --selector=app.kubernetes.io/component=controller \
+  --timeout=15m
+kubectl apply -f ingress.yaml
