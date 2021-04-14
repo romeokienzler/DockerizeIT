cf94fe258ae76a70aee368f3d114061ae4737e5a
root <root@rkie-think21-elyra.IBM.cloud>
2021-04-14 15:37:01 +0000

    latest shit

--- elyra-ai/deploy.sh
+++ elyra-ai/deploy.sh
@@ -1,3 +1,4 @@
+#wget https://dl.k8s.io/v1.21.0/kubernetes-client-linux-amd64.tar.gz
 #curl -Lo ./kind https://kind.sigs.k8s.io/dl/v0.10.0/kind-linux-amd64
 #chmod +x ./kind
 #mv ./kind /bin/
@@ -11,4 +12,5 @@ kubectl wait --namespace ingress-nginx \
   --for=condition=ready pod \
   --selector=app.kubernetes.io/component=controller \
   --timeout=15m
-kubectl apply -f ingress.yaml
+kubectl apply -f ingress_vhost.yaml
+kubectl apply -f ingress-kfp.yaml -n kubeflow
