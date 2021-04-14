5451c6ef9e21e2f6f11a01949002d2882337be27
Romeo Kienzler <romeo.kienzler@ch.ibm.com>
2021-04-14 15:50:49 +0200

    using vhosts

--- elyra-ai/deploy.sh
+++ elyra-ai/deploy.sh
@@ -1,3 +1,6 @@
+#curl -Lo ./kind https://kind.sigs.k8s.io/dl/v0.10.0/kind-linux-amd64
+#chmod +x ./kind
+#mv ./kind /bin/
 #kind create cluster --config=kind.yaml
 kubectl apply -f pv.yaml
 kubectl apply -f pvc.yaml
