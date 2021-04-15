abe4396d50dcd7ff8df6c02da6318167dfc1d456
Romeo Kienzler <romeo.kienzler@gmail.com>
2021-04-15 15:41:49 +0200

    Update deploy.sh

--- elyra-ai/deploy.sh
+++ elyra-ai/deploy.sh
@@ -3,6 +3,10 @@
 #chmod +x ./kind
 #mv ./kind /bin/
 #kind create cluster --config=kind.yaml
+export PIPELINE_VERSION=1.4.1
+kubectl apply -k "github.com/kubeflow/pipelines/manifests/kustomize/cluster-scoped-resources?ref=$PIPELINE_VERSION"
+kubectl wait --for condition=established --timeout=60s crd/applications.app.k8s.io
+kubectl apply -k "github.com/kubeflow/pipelines/manifests/kustomize/env/platform-agnostic-pns?ref=$PIPELINE_VERSION"
 kubectl apply -f pv.yaml
 kubectl apply -f pvc.yaml
 kubectl apply -f pod_pvc.yaml
