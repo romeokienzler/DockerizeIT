5fc0c1dba54eca7f89306195a1b21d456a920368
Romeo Kienzler <romeo.kienzler@ch.ibm.com>
2021-04-17 12:11:47 +0200

    added wsjtx

---
+++ wsjtx/run.sh
@@ -0,0 +1,4 @@
+sudo xhost +
+source ../env
+docker run -it --net=host --ipc=host -e DISPLAY=$DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix --env="QT_X11_NO_MITSHM=1"  --device /dev/snd wsjtx /bin/bash
+
