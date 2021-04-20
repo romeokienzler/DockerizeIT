63681ba18a05aa3d611e21e63e3b15f0e3830acf
Romeo Kienzler <romeo.kienzler@gmail.com>
2021-04-20 14:54:14 +0200

    Update deploy.sh

--- elyra-ai/deploy.sh
+++ elyra-ai/deploy.sh
@@ -1,7 +1,3 @@
-kubectl apply -f pv.yaml
-kubectl apply -f pvc.yaml
-kubectl apply -f pod_pvc.yaml
-kubectl apply -f service.yaml
 kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/master/deploy/static/provider/kind/deploy.yaml
 kubectl wait --namespace ingress-nginx \
   --for=condition=ready pod \
