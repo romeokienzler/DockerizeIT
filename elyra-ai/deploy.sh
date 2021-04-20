0710acdf9772083d5d3da4e7b0fd135a441a0d9f
Romeo Kienzler <romeo.kienzler@gmail.com>
2021-04-20 15:01:50 +0200

    Update and rename deploy.sh to configure_ingress.sh

--- elyra-ai/deploy.sh
+++
@@ -1,7 +0,0 @@
-kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/master/deploy/static/provider/kind/deploy.yaml
-kubectl wait --namespace ingress-nginx \
-  --for=condition=ready pod \
-  --selector=app.kubernetes.io/component=controller \
-  --timeout=15m
-kubectl apply -f ingress_vhost.yaml
-kubectl apply -f ingress-kfp.yaml -n kubeflow
