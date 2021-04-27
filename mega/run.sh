b6790a20da328adccb60ff96f582d67f652a2e09
Romeo Kienzler <romeo.kienzler@ch.ibm.com>
2021-04-27 17:29:54 +0200

    mega

--- mega/run.sh
+++ mega/run.sh
@@ -1,2 +1,2 @@
-docker run -it -v /home/romeokienzler/Downloads/2mega:/shared --workdir /shared  --workdir /shared mega
+docker run -it -v /home/romeokienzler/Downloads/mega:/shared --workdir /shared  --workdir /shared mega
 
