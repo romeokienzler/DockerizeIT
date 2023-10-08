cc83417386ded3711a14dfc072d252c8f11d544f
Romeo Kienzler <romeo.kienzler1@ibm.com>
2023-10-08 12:02:39 +0200

    commit latest status

---
+++ aeolus/run.sh
@@ -0,0 +1,8 @@
+sudo xhost +
+#docker run -it --net=host --ipc=host -e DISPLAY=$DISPLAY -v /tmp/shared:/shared --workdir /shared -v /tmp/.X11-unix:/tmp/.X11-unix --env="QT_X11_NO_MITSHM=1" --device /dev/snd  audacity
+docker run -it --net=host --ipc=host -e DISPLAY=$DISPLAY \
+    -e PULSE_SERVER=unix:${XDG_RUNTIME_DIR}/pulse/native \
+    -v ${XDG_RUNTIME_DIR}/pulse/native:${XDG_RUNTIME_DIR}/pulse/native \
+    -v ~/.config/pulse/cookie:/root/.config/pulse/cookie \
+    --group-add $(getent group audio | cut -d: -f3) -v /tmp/.X11-unix:/tmp/.X11-unix --env="QT_X11_NO_MITSHM=1" --device /dev/snd  aeolus /bin/bash
+
---
+++ siemens_logo/build.sh
@@ -0,0 +1,2 @@
+docker build -t siemens_logo .
+
