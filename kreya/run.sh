991e77c9a762ab637aafd5f62fc91d16b012e5bf
Romeo Kienzler <romeo.kienzler1@ibm.com>
2023-10-08 12:04:19 +0200

    Revert "commit latest status"
    This reverts commit cc83417386ded3711a14dfc072d252c8f11d544f.

--- kreya/run.sh
+++
@@ -1,4 +0,0 @@
-sudo xhost +
-source ../env
-docker run -it --net=host --ipc=host -e DISPLAY=$DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix --env="QT_X11_NO_MITSHM=1" kreya /bin/bash
-
--- sidequest/run.sh
+++
@@ -1,7 +0,0 @@
-#sudo xhost +
-#source ../env
-#docker run -it --net=host --ipc=host -e DISPLAY=$DISPLAY -v /tmp/shared:/shared --workdir /shared -v /tmp/.X11-unix:/tmp/.X11-unix --env="QT_X11_NO_MITSHM=1" --device /dev/snd  audacity
-#docker run -it --net=host --ipc=host -e DISPLAY=$DISPLAY -v $arduino_root:/root --workdir /root --device=/dev/ttyUSB0 -v /tmp/.X11-unix:/tmp/.X11-unix --env="QT_X11_NO_MITSHM=1" arduino
-#docker run -it --net=host --ipc=host -e DISPLAY=$DISPLAY --device=/dev/ttyUSB0 -v /tmp/.X11-unix:/tmp/.X11-unix --env="QT_X11_NO_MITSHM=1" sidequest /SideQuest-0.10.36/sidequest --no-sandbox
-docker run -it --net=host --ipc=host -e DISPLAY=$DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix --env="QT_X11_NO_MITSHM=1" sidequest /SideQuest-0.10.36/sidequest --no-sandbox
-
