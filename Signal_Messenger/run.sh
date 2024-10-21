1292a18dc1c9c0aec08df1cfe04b81a2a24f87ee
lange <lange@synyx.de>
2024-10-21 22:44:08 +0200

    only allow access to local docker

--- Signal_Messenger/run.sh
+++ Signal_Messenger/run.sh
@@ -1,4 +1,4 @@
-sudo xhost +
+sudo xhost +local:docker
 source ../env
 docker run -d --net=host --ipc=host -e DISPLAY=$DISPLAY -v /home/romeokienzler/Downloads:/shared -v $signal_root:/root -v /tmp/.X11-unix:/tmp/.X11-unix --device /dev/snd --env="QT_X11_NO_MITSHM=1" signal
 
